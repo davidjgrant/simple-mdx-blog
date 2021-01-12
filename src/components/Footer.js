59923726862f6a74f84024082a0043d035493115
David Grant <davidgrant132@gmail.com>
2021-01-12 21:06:58 +0000

    add footer & social icons

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { FooterSocialIcons, FooterSocialWrapper, FooterWrapper } from "../elements"
+
+export const Footer = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            facebook: file(relativePath: {eq: "facebook.svg"}) {
+                publicURL
+            }
+            instagram: file(relativePath: {eq: "instagram.svg"}) {
+                publicURL
+            }
+            linkedin: file(relativePath: {eq: "linkedin.svg"}) {
+                publicURL
+            }
+            twitter: file(relativePath: {eq: "twitter.svg"}) {
+                publicURL
+            }
+        }
+    `)
+
+    return (
+        <FooterWrapper>
+            <FooterSocialWrapper>
+                <FooterSocialIcons>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.facebook.publicURL} alt="Facebook Logo" />
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.instagram.publicURL} alt="instagram Logo" />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.linkedin.publicURL} alt="linkedin Logo" />
+                    </a>
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                        <img src={data.twitter.publicURL} alt="twitter Logo" />
+                    </a>
+                </FooterSocialIcons>
+                <p>© 2021 Company, Inc. All rights reserved.</p>
+            </FooterSocialWrapper>
+        </FooterWrapper>
+    )
+}
