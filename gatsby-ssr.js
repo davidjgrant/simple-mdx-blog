e7b4143eb612ddf24e97ee772ccc2b0095245fda
David Grant <davidgrant132@gmail.com>
2021-01-16 19:43:19 +0000

    style mdx files and prism syntax highligher

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import Theme from './src/themes/theme'
+import { MDXProvider } from "@mdx-js/react"
+import { preToCodeBlock } from "mdx-utils"
+import Theme from "./src/themes/theme"
+import { Table, Code } from "./src/components"
+import "./language-tabs.css"
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -16,9 +20,24 @@ const GlobalStyle = createGlobalStyle`
     }
 `
 
+const components = {
+    table: Table,
+    pre: preProps => {
+        const props = preToCodeBlock(preProps)
+        if (props) {
+            return <Code {...props} />
+        }
+
+        return <pre {...preProps} />
+    },
+    wrapper: ({ children }) => <>{children}</>,
+}
+
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={Theme}>
-        <GlobalStyle />
-        {element}
-    </ThemeProvider>
+    <MDXProvider components={components}>
+        <ThemeProvider theme={Theme}>
+            <GlobalStyle />
+            {element}
+        </ThemeProvider>
+    </MDXProvider>
 )
---
+++ src/components/Table.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { TableWrapper } from "../elements"
+
+export const Table = ({children}) => {
+    return (
+        <TableWrapper><table>{children}</table></TableWrapper>
+    )
+}
